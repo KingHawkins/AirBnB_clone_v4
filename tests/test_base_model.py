9101cbc977469e5c221fa229ffb95b47058eb93b
KingHawkins <kinghawkins62@gmail.com>
2023-02-11 15:41:18 +0300

    adding tests

---
+++ tests/test_base_model.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+"""
+This module contains the TestBaseModel class
+"""
+from models.base_model import BaseModel
+from datetime import datetime
+import unittest
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    all unit test cases for the BaseModel class
+    """
+    def setUp(self):
+        """
+        create instances of the BaseModel class
+        """
+        self.bm = BaseModel()
+
+    def tearDown(self):
+        """
+        deletes all instances created at exit
+        """
+        del self.bm
+
+    def test_init_(self):
+        """
+        unit test for the __init__ method
+        """
+        self.assertTrue(self.bm.id)
+        self.assertIsInstance(self.bm.created_at, datetime)
+        self.assertIsInstance(self.bm.updated_at, datetime)
+
+        dic = self.bm.to_dict()
+        new = BaseModel(**dic)
+        new.name = "Joy"
+        new.my_number = 7
+        new.save()
+        self.assertIsInstance(new, BaseModel)
+        self.assertEqual(new.my_number, 7)
+        self.assertIsInstance(new.created_at, datetime)
+        self.assertIsInstance(new.updated_at, datetime)
+
+    def test_str(self):
+        """ unit test for the string representation"""
+        eo = f"[{self.bm.__class__.__name__}] ({self.bm.id})"
+        eo_c = f" {self.bm.__dict__}"
+
+        self.assertEqual(str(self.bm), eo + eo_c)
+
+    def test_to_dict(self):
+        """test the dictionary method"""
+        dic = self.bm.to_dict()
+        self.assertIsInstance(dic, dict)
+        self.assertIsInstance(dic['updated_at'], str)
+        self.assertIsInstance(dic['created_at'], str)
+
+    def test_save(self):
+        """ test the save method"""
+        pre = self.bm.updated_at
+        self.bm.save()
+
+    def test_instantiation(self):
+        """tests all instantiation of BaseModel"""
+        self.assertIsInstance(self.bm, BaseModel)
+        self.assertIsNotNone(self.bm.id)
+
+    def test_instance_updated_at(self):
+        """tests all instance of updated_at of BaseModel"""
+        self.assertIsNotNone(self.bm.updated_at)
+
+    def test_instance_created_at(self):
+        """tests all instance created_at of BaseModel"""
+        self.assertIsNotNone(self.bm.created_at)
+
+    def test_str_representation(self):
+        """Tests printing string representation"""
+        e = f"[{self.bm.__class__.__name__}] ({self.bm.id}) {self.bm.__dict__}"
+        self.assertEqual(str(self.bm), e)
+
+    def test_to_dict_classObj(self):
+        """Tests for class in dictionary convertion"""
+        dic_obj = self.bm.to_dict()
+        self.assertIn('__class__', dic_obj)
+        self.assertEqual(dic_obj['__class__'], 'BaseModel')
+
+    def test_to_dict_createdAt(self):
+        """Tests for created_at in dictionary convertions"""
+        dic_obj = self.bm.to_dict()
+        self.assertIn('created_at', dic_obj)
+        self.assertIsInstance(dic_obj['created_at'], str)
+
+    def test_to_dict_updated_at(self):
+        """Tests for updated_at in dictionary convertions"""
+        dic_obj = self.bm.to_dict()
+        self.assertIn('updated_at', dic_obj)
+        self.assertIsInstance(dic_obj['updated_at'], str)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_engine/test_file_storage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Test suits for FileStorage class"""
+
+import unittest
+import json
+from io import StringIO
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+
+class TestFileStorage(unittest.TestCase):
+    """Tests methods in FileStorage class"""
+
+    def setUp(self):
+        """Sets up instances needed for tests"""
+        self.storage = FileStorage()
+        self.model = BaseModel()
+        self.e = f'{self.model.__class__.__name__}.{self.model.id}'
+
+    def test_all(self):
+        """Tests for the all method"""
+        self.storage.new(self.model)
+        result = self.storage.all()
+        self.assertIsInstance(result, dict)
+        self.assertIn(self.e, result)
+
+    def test_new(self):
+        """Tests for the new method"""
+        self.storage.new(self.model)
+        result = self.storage.all()
+        self.assertIn(self.e, result)
+
+    def test_save(self):
+        """Tests for the save method"""
+        self.storage.new(self.model)
+        old_content = open('file.json', 'r').read()
+        self.storage.save()
+        new_content = open('file.json', 'r').read()
+        self.assertNotEqual(old_content, new_content)
+
+    def test_reload(self):
+        """Tests for the reload method"""
+        self.storage.new(self.model)
+        self.storage.save()
+        self.storage.reload()
+        result = self.storage.all()
+        self.assertIn(self.e, result)
