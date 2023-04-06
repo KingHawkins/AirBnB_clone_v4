b9cfe5be0a758a12ad7a39123d53378328614b23
KingHawkins <Kinghawkins61@gmail.com>
2023-04-06 15:40:59 +0300

    update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,19 +3,22 @@
 from datetime import datetime
 from fabric.api import local
 from shlex import split
-import os
 
 
 def do_pack():
     """Creates an archive file"""
     try:
+        """Under surveillance\
+                might fail"""
         result = split(str(datetime.now()))
         (yr, mon, day) = result[0].split('-')
         (hr, mn, sec) = result[1].split('.')[0].split(':')
         file = f'web_static_{yr}{mon}{day}{hr}{mn}{sec}.tgz'
-        if not os.path.exists('versions'):
-            os.mkdir('versions')
+        local('mkdir -p versions')
         local(f'tar -czf versions/{file} web_static')
         return 'versions/' + file
     except Exception:
         return None
+
+
+do_pack()
