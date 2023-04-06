65fb9a63eaf87fa28224941a29d8f14706630f81
KingHawkins <Kinghawkins61@gmail.com>
2023-04-06 16:26:28 +0300

    updates

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-# sets up web server
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
-echo "<h1>Testing Deployment</h1>" > /data/web_static/releases/test/index.html
+# Script that configures Nginx server with some folders and files
+apt-get -y update
+apt-get -y install nginx
+service nginx start
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown ubuntu:ubuntu /data/
-sed -i '\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+chown -R ubuntu:ubuntu /data/
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
