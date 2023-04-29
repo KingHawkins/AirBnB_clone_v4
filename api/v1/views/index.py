84e5cae7799ddc427f302cef46474b54f7424eff
KingHawkins <Kinghawkins61@gmail.com>
2023-04-29 23:46:01 +0300

    updating index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,22 +8,22 @@ Usage:
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def static():
     """returns json object"""
     return jsonify(status="OK")
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'])
 def stats():
     """Returns the count for all items in database"""
     amenities = storage.count(Amenity)
